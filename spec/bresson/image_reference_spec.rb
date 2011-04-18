f97e9d866e9fe1abdb2911172be9a1a10caf1d61
Mortaro <mortaro@nChan.(none)>
2011-04-18 15:43:30 -0300

    adding rspec

---
+++ spec/bresson/image_reference_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+describe ImageReference do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'bresson'
+
+RSpec.configure do |config|
+end
