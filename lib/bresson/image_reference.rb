8a23c3e2e1567ce7c26905ee37526a18cafce92a
unknown <Mortaro@dChan.(none)>
2012-11-09 11:54:53 -0200

    added legend and thumb support

--- lib/bresson/image_reference.rb
+++ lib/bresson/image_reference.rb
@@ -1,14 +1,19 @@
 module Bresson
   class ImageReference
-    attr_accessor :item, :link
+    attr_accessor :item, :link, :legend
 
     def initialize args
       @item = eval "Bresson::#{args[:source]}.new"
       @link = args[:link]
+      @legend = args[:legend]
     end
 
     def method_missing name, *args
       eval "@item.#{name}(args)"
+    end
+
+    def thumb
+      @link.split(".com/").join(".com/thumb/")
     end
 
     def to_s
--- lib/bresson/version.rb
+++ lib/bresson/version.rb
@@ -1,3 +1,3 @@
 module Bresson
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
